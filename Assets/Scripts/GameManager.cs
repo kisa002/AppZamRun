ed01c6be8421a80585b2b43bf830303060d6afd0
Holy-Knight <vnycall74@naver.com>
2017-04-01 20:11:24 +0900

    타임 게이지 추가

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class GameManager : MonoBehaviour {
 
 	public bool stop = false;
+	public bool gameOver = false;
+
+	public float time = 60.0f;
 
 	// Use this for initialization
 	void Start () {
