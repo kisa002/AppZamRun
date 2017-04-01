3e8d84f9e38992e4c292e19dfab3e47150700f22
Holy-Knight <vnycall74@naver.com>
2017-04-02 05:41:56 +0900

    Intro 추가
    인트로 끝나면 취할 액션 추가해야함

---
+++ Assets/Scripts/IntroCamera.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IntroCamera : MonoBehaviour {
+
+	bool run = true;
+
+	void Start () {
+		StartCoroutine ("Home");	
+	}
+	
+	IEnumerator Home()
+	{
+		while (run) {
+			var pos = transform.position;
+			transform.position = new Vector3 (pos.x, pos.y - 0.03f, pos.z);
+
+			if (transform.position.y <= -6.57)
+				run = false;
+			
+			yield return new WaitForSeconds (0.01f);
+		}
+
+		yield return new WaitForSeconds (2.0f);
+
+
+	}
+}
